2897ff3e11507fb5413c3a38fdc875f1227bb9db
Ces <francesluis23@gmail.com>
2024-10-26 15:54:25 +0800

    fixed contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,41 +1,30 @@
 
 import styles from '../assets/styles/contact.module.scss';
-import ContactImage from '/images/contact-header.png';
+import ContactImage from '/images/contact.webp';
 import ContactImage2 from '/images/contact image.jpg';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState, useContext } from 'react';
 import axios from '../api/axios';
 import Header from '../components/header'
+import Footer from '../components/footer';
+
+const container =[
+  {role:'Team Lead / Frontend Dev Lead', name:'Bryan Aaron Santiago', email:'santiagobryan831@gmail.com'},
+  {role:'Backend Dev Lead', name:'Renell Constantino', email:'rmarioanoconstantino@gmail.com'},
+  {role:'Frontend Dev', name:'Frances Tumampos', email:'tumamposfrances@gmail.com'},
+  {role:'Frontend Dev', name:'Cayoh Anicete', email:'cayohallouu@gmail.com'},
+  {role:'Frontend Dev', name:'Vladimir Borja', email:'vladimirborja013@gmail.com'}
+]
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    fName: '',
-    lName: '',
-    phonenumber: '',
-    email: '',
-    subject: '',
-    message: '',
-  });
-
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     try {
       await axios.post('/auth/register', formData);
-      alert('successful!');
-      setFormData({ fName: '', lName: '', phonenumber: '', email: '', subject: '', message: '', }); // Reset form
-      
+      alert('successful!');      
     } catch (err) {
       console.error(err);
       alert(err.response?.data?.message || 'please check your information');
@@ -45,91 +34,83 @@ const Contact = () => {
   };
 
   return (
-    <>
+    <main>
       <Header />
-    <main  className={styles.header}>
-      <img src={ContactImage} alt="Contact" />
-      <div className={styles.content__container}>
-        <h1  className={styles.h1}> Get in touch. </h1>
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <label  className={styles.fname}>First Name</label>
-          <label>
-            <input name="fName" value={formData.fName} onChange={handleChange} type="text" required />
-          </label>
-          <label>Last Name </label>
-          <label>
-            <input name="lName" value={formData.lName} onChange={handleChange} type="text" required />
-          </label>
-          <label>Email address </label>
-          <label>
-            <input name="email" value={formData.email} onChange={handleChange} type="email" required />
-          </label>
-          <label>Phone Number </label>
-          <label>
-            <input name="phonenumber" value={formData.phonenumber} onChange={handleChange} type="text" required />
-          </label>
-          <label>Subject </label>
-          <label>
-            <input name="subject" value={formData.subject} onChange={handleChange} type="text" required />
-          </label>
-          <label>Message </label>
-          <label className={styles.message}>
-            <input name="message" value={formData.message} onChange={handleChange} type="text" required />
-          </label>
-          <div className={styles.container2}>
-            <button type="submit" className={styles.submit__button} disabled={loading}>
-             {loading ? 'Submitting...' : 'Submit'} 
-            </button>
-              <button type="delete" className={styles.delete__button} disabled={loading}>
-              {loading ? 'Deleting ...' : 'Delete'} 
-            </button>
+      <div className={styles.container}>  
+          <img src={ContactImage} className={styles.landing} alt="Contact" /> 
+          <div className={styles.img_container}>
+            <h1>Find your perfect home with us</h1>      
+          </div> 
+        <section className={styles.content}>
+          <div className={styles.about_container}>
+            <span className={styles.category}>[REACT OUT]</span>
+            <h1 className={styles.title}>If you're excited to experience Crown Residence, feel free to get in touch! We’d love to hear from you about your stay plans. Give us a call or send us an email. You can find the contact details of the best person to reach out to below, and we’ll get back to you as soon as possible.</h1>
           </div>
-        </form>
+        </section>
 
-        <div className={styles.container2}>
-            <button type="" className={styles.card} >
-              <i class="ri-map-pin-line ri-3x"></i>
-              <h1> Visit Us </h1> <br></br>
-              <p>Unit 1 Ground Floor, Acacia Building, 
-                  Sto. Rosario, Angeles City, 
-                  Pampanga, Philippines</p>
-            </button>
+          <div className={styles.cards}>
 
-            <button type="" className={styles.card} >
-              <i class="ri-phone-line  ri-3x"></i>
-              <h1> Call Us </h1><br></br>
-              <p>(+63) 912 345 67</p>
-            </button>
+            <section className={styles.contact}>
+              <img src="/images/2.webp" alt="" />
+              <div className={styles.card_content}>
+                <div>
+                  <p className={styles.card_category}>[PEOPLE]</p>
+                  <h1 className={styles.card_title}>CONTACT</h1>
+                </div>
+                {container.map((content) => (
+                  <div>
+                    <p className={styles.title}>{content.role}</p>
+                    <p className={styles.name}>{content.name}</p>
+                    <a className={styles.link} href={`mailto:content.email`}>{content.email}</a>                  
+                  </div>
+                ))}
+              </div>
+            </section>
 
-            <button type="" className={styles.card} >
-            <i class="ri-mail-open-line  ri-3x"></i>
-              <h1> Send an email </h1><br></br>
-              <p>crownresidence@email.com</p>
-            </button>
-          </div>
-      </div>
+            <section className={styles.navigation}>
+              <img src="/images/2.webp" alt="" />
+              <article className={styles.card_content}>
+                <div>
+                  <p className={styles.card_category}>[WEBSITE]</p>
+                  <h1 className={styles.card_title}>NAVIGATION</h1>
+                </div>
+                <div>
+                  <p className={styles.title}>Pages</p>
+                  <a href=''>Home</a>                  
+                  <a href=''>About Us</a>                  
+                  <a href=''>Property</a>                  
+                  <a href=''>Blog</a>                  
+                  <a href=''>Reserved Property</a> 
+                </div>                
+                <div>
+                  <p className={styles.title}>Details</p>
+                  <a href=''>+639282977154</a>                  
+                  <a href=''>crownresidence@hau.dev</a>                                 
+                </div>
+              </article>
+            </section>
 
-      <div className={styles.content__container2}>
-        <img src={ContactImage2} alt="Contact" />
-        <div>
-        <h1 className={styles.h11}>Subscribe to our Newsletter.</h1>
-          <p className={styles.p1}>Stay informed with the latest updates, exclusive discounts, promotions, and exciting offers by subscribing to our newsletter. Join our community to receive timely information and special deals directly to your inbox!</p>
-          <form className={styles.form3} onSubmit={handleSubmit}>
-            <label>Email address </label>
-              <label>
-                <input name="email" value={formData.email} onChange={handleChange} type="email" required />
-            </label>
-          </form>
-          <div className={styles.container2}>
-            <button type="subscribe" className={styles.submit__button2} disabled={loading}>
-            {loading ? 'Subscribing...' : 'Subscribe Now'} 
-            </button>
-          </div>
+            <section className={styles.socials}>
+              <img src="/images/2.webp" alt="" />
+              <article className={styles.card_content}>
+                <div>
+                  <p className={styles.card_category}>[WEBSITE]</p>
+                  <h1 className={styles.card_title}>NAVIGATION</h1>
+                </div>
+                <div>
+                  <p className={styles.title}>Networks</p>
+                  <a href=''>Youtube</a>                  
+                  <a href=''>X</a>                  
+                  <a href=''>Facebook</a>                  
+                  <a href=''>LinkedIn</a>                  
+                  <a href=''>Instagram</a>   
+                </div>              
+              </article>
+            </section>
           </div>
       </div>
-      <br></br>
+      <Footer />
     </main>
-    </>
   );
 };
 
